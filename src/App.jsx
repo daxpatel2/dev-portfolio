7e25e63c4830da73dca1c986321ee8e723d8b26f
UnmarkedBanshee <dxp38@pct.edu>
2023-06-23 11:15:22 -0400

    prettier code format doing its magic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from './components/Header/header';
-import Home from './components/Home/home';
-import About from './components/About/About';
-import Contacts from './components/Contacts/Contacts';
-import Works from './components/Projects/Projects';
-import Scroll from './components/ScrollUp/Scroll';
+import "./App.css";
+import Header from "./components/Header/header";
+import Home from "./components/Home/home";
+import About from "./components/About/About";
+import Contacts from "./components/Contacts/Contacts";
+import Works from "./components/Projects/Projects";
+import Scroll from "./components/ScrollUp/Scroll";
 
 function App() {
   return (
@@ -20,7 +19,7 @@ function App() {
       </main>
       <Scroll />
     </>
-  )
+  );
 }
 
 export default App;
