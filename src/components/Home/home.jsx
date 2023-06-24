f0f08c0acdadd5a950ad239c1895ab9bf28d9c22
UnmarkedBanshee <dxp38@pct.edu>
2023-06-24 01:22:41 -0400

    view models changed

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import Social from "./Social";
-import './home.css';
-import Data from './Data';
+import "./home.css";
+import Data from "./Data";
 
 function Home() {
-    return (
-        <section className="home section" id='home'>
-            <div className="home__container container grid">
-                <div className="home__content grid">
-                    <Social />
-                    <Data />
-                    <div className="home__img">
-                    </div>
-                </div>
-                <br/>
-                <br/>
-            </div>
-        </section>
-    )
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
+          <Data />
+          <div className="home__img"></div>
+        </div>
+        <br />
+        <br />
+      </div>
+    </section>
+  );
 }
 
 export default Home;
