c979c0adff9a4f5b6dfb3aca9aa68fe5af79d8b9
UnmarkedBanshee <dxp38@pct.edu>
2023-07-16 14:40:19 -0400

    updated projects sections

--- src/components/About/Tech.jsx
+++ src/components/About/Tech.jsx
@@ -1,55 +1,56 @@
-import React from 'react'
-import './Tech.css'
-import CodeIcon from '@mui/icons-material/Code';
-import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
-import StorageIcon from '@mui/icons-material/Storage';
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import React from "react";
+import "./Tech.css";
+import CodeIcon from "@mui/icons-material/Code";
+import ComputerOutlinedIcon from "@mui/icons-material/ComputerOutlined";
+import StorageIcon from "@mui/icons-material/Storage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faDatabase, faRuler } from '@fortawesome/fontawesome-free-solid'
-import { faCode } from '@fortawesome/free-solid-svg-icons';
-import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 
 function Tech() {
   return (
     <div className="h2">
-        <h2 className='h22'>TECHNOLOGIES I'VE USED</h2>
-        <section class='tech-container'>
-            <div className="card">
-                    <div className="i">
-                        <CodeIcon fontSize='medium' className='icon'/>
-                    </div>
-                    <h2>BACK END</h2>
-                    <p>C</p>
-                    <p>Java</p>
-                    <p>Python</p>
-                    <p>NodeJS</p>
-                    <p>ExpressJS</p>
+      <h2 className="h22">TECHNOLOGIES I'VE USED</h2>
+      <section class="tech-container">
+        <div className="card">
+          <div className="i">
+            <CodeIcon fontSize="medium" className="icon" />
+          </div>
+          <h2>BACK END</h2>
+          <p>C</p>
+          <p>Java</p>
+          <p>Python</p>
+          <p>NodeJS</p>
+          <p>ExpressJS</p>
+        </div>
+        <div className="card">
+          <div className="card-image">
+            <div className="i">
+              <ComputerOutlinedIcon fontSize="medium" className="icon" />
             </div>
-            <div className="card">
-                <div className="card-image">
-                    <div className="i">
-                        <ComputerOutlinedIcon fontSize='medium' className='icon'/>
-                    </div>
-                    <h2>FRONT END</h2>
-                    <p>CSS</p>
-                    <p>React</p>
-                    <p>HTML</p>
-                    <p>NextJS</p>
-                    <p>JavaScript</p>
-                </div>
+            <h2>FRONT END</h2>
+            <p>CSS</p>
+            <p>React</p>
+            <p>HTML</p>
+            <p>NextJS</p>
+            <p>JavaScript</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-image">
+            <div className="i">
+              <StorageIcon fontSize="medium" className="icon" />
             </div>
-            <div className="card">
-                <div className="card-image">
-                    <div className="i">
-                        <StorageIcon fontSize='medium' className='icon'/>
-                    </div>
-                    <h2>DATABASE</h2>
-                    <p>SQL</p>
-                    <p>Firebase Firestore</p>
-                </div>
-            </div>
-        </section>
+            <h2>DATABASE</h2>
+            <p>SQL</p>
+            <p>Firebase Firestore</p>
+            <p>Appwrite Database</p>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
 export default Tech;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,92 +1,92 @@
-import React from 'react';
-import './Projects.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
-import { projects } from '../constants/';
+import React from "react";
+import "./Projects.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { projects } from "../constants/";
+import twitterGif from "../../assets/twitter.gif";
+import trelloGif from "../../assets/trello.gif";
+import fileGif from "../../assets/file.gif";
 
-const twitterTech =[
-    "React",
-    "CSS",
-    "Firebase"
-]
+const twitterTech = ["React", "CSS", "Firebase"];
 
-const hackBank = [
-    "NextJS",
-    "Tailwind CSS",
-    "Express",
-    "Firebase"
-]
+const hackBank = ["NextJS", "Tailwind CSS", "Express", "Firebase"];
 
+const File_System = ["C", "SQL", "Linux", "Bash"];
 
-const JPMC = [
-    "React",
-    "Python",
-    "CSS"
-]
-
-function Projects({name, description, tags, video, source_code_link}) {
+function Projects({ name, description, gif, tags, source_code_link }) {
   return (
     <section className="project__container" id="projects">
-        <div className="image-video">
-            <video width="500" height="350" controls className='video'>
-                <source src={video} type="video/mp4"/>
-            </video>
+      <div className="image-video">
+        <img src={gif} alt="image" />
+        {/* <video width="500" height="350" controls className="video">
+          <source src={video} type="video/mp4" />
+        </video> */}
+      </div>
+      <div className="about">
+        <div className="project-hero">
+          <p className="featured">Featured</p>
+          <a href={source_code_link} target="_blank" className="link">
+            {name}
+          </a>
         </div>
-        <div className="about">
-            <div className="project-hero">
-                <p className='featured'>Featured</p>
-                <a href={source_code_link} target='_blank' className="link">{name}</a>
-            </div>
-            <div className="about_text">
-                <p>{description}</p>
-            </div>
-            <div className="more">
-                <div className="tech-tech">
-                    {tags?.map((technology) => (
-                        <li className='listy' key={technology}>{technology}</li>
-                    ))}
-                </div>
-                <a href={source_code_link} className="source-link" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faCode} /></a>
-            </div>
+        <div className="about_text">
+          <p>{description}</p>
         </div>
+        <div className="more">
+          <div className="tech-tech">
+            {tags?.map((technology) => (
+              <li className="listy" key={technology}>
+                {technology}
+              </li>
+            ))}
+          </div>
+          <a
+            href={source_code_link}
+            className="source-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faCode} />
+          </a>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 function Works() {
-    return (
-        <section className="works">
-            <h2 className="my-projects">
-                PROJECTS
-            </h2>
-            <div className='project-wrapper'>
-                <Projects
-                    name={projects[0].name}
-                    description={projects[0].description}
-                    tags={twitterTech}
-                    video={projects[0].video}
-                    source_code_link={projects[0].source_code_link}
-                />
-                <Projects
-                    name={projects[1].name}
-                    description={projects[1].description}
-                    tags={hackBank}
-                    video={projects[1].video}
-                    source_code_link={projects[1].source_code_link}
-                />
-                <Projects
-                    name={projects[2].name}
-                    description={projects[2].description}
-                    tags={JPMC}
-                    video={projects[2].video}
-                    source_code_link={projects[2].source_code_link}
-                />
-            </div>
-            <p className='archive'>ALL PROJECTS</p>
-            <a className='view-arc' href='/archive'>View Archive</a>
-
-        </section>
-    )
+  return (
+    <section className="works">
+      <h2 className="my-projects">PROJECTS</h2>
+      <div className="project-wrapper">
+        <Projects
+          name={projects[0].name}
+          description={projects[0].description}
+          tags={twitterTech}
+          gif={twitterGif}
+          source_code_link={projects[0].source_code_link}
+        />
+        <Projects
+          name={projects[1].name}
+          description={projects[1].description}
+          tags={hackBank}
+          gif={trelloGif}
+          source_code_link={projects[1].source_code_link}
+        />
+        <Projects
+          name={projects[2].name}
+          description={projects[2].description}
+          tags={File_System}
+          gif={fileGif}
+          source_code_link={projects[2].source_code_link}
+        />
+      </div>
+      <p className="archive">ALL PROJECTS</p>
+      <a className="view-arc" href="/archive">
+        View Archive
+      </a>
+    </section>
+  );
 }
 
 export default Works;
